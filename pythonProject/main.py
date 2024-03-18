d5b7c055fcac647739d60b6819dc64d0d247b330
janjurinoko@gmail.com <61251392+InYanInIn@users.noreply.github.com>
2024-03-18 12:49:43 +0100

    Task4 and Task5

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,10 +1,27 @@
-squaresList = [(n ** 2) for n in range(1, 11)]
-print(squaresList)
+import math
+
+class SquareGenerator:
+    def getSquaresForRange(start, end):
+        if start > end:
+            print('Start cant be bigger then end')
+            return
+        else:
+            return [(n ** 2) for n in range(start, end + 1)]
+
+    def getSquareRootsFromList(list):
+        return [math.sqrt(n) for n in list]
+
 
 def getSquaresRange(start, end):
     return [(n ** 2) for n in range(start, end + 1)]
 
 
-class SquareGenerator:
-    def getSquaresForRange(start, end):
-        return [(n ** 2) for n in range(start, end + 1)]
+squaresList = [(n ** 2) for n in range(1, 11)]
+
+print(squaresList)
+print(SquareGenerator.getSquaresForRange(1,11))
+print(SquareGenerator.getSquareRootsFromList(squaresList))
+
+
+
+
