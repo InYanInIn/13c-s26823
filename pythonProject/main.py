fc2fe2b7add5776556ae22f95f69a8cc0afff638
janjurinoko@gmail.com <61251392+InYanInIn@users.noreply.github.com>
2024-03-18 12:42:09 +0100

    Task3

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,7 +1,10 @@
 squaresList = [(n ** 2) for n in range(1, 11)]
-
+print(squaresList)
 
 def getSquaresRange(start, end):
     return [(n ** 2) for n in range(start, end + 1)]
 
 
+class SquareGenerator:
+    def getSquaresForRange(start, end):
+        return [(n ** 2) for n in range(start, end + 1)]
