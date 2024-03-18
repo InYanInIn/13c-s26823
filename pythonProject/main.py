a14200fa8658c20f244707a5190cbe0886a7ce6c
janjurinoko@gmail.com <61251392+InYanInIn@users.noreply.github.com>
2024-03-18 12:57:17 +0100

    Task8

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -2,6 +2,14 @@ import math
 from square_generator import *
 
 
+class CubicGenerator(SquareGenerator):
+    def getSquaresForRange(start, end):
+        if start > end:
+            print('Start cant be bigger then end')
+            return
+        else:
+            return [(n ** 3) for n in range(start, end + 1)]
+
 def getSquaresRange(start, end):
     return [(n ** 2) for n in range(start, end + 1)]
 
@@ -11,6 +19,7 @@ squaresList = [(n ** 2) for n in range(1, 11)]
 print(squaresList)
 print(SquareGenerator.getSquaresForRange(1,10))
 print(SquareGenerator.getSquareRootsFromList(squaresList))
+print(CubicGenerator.getSquaresForRange(1,10))
 
 
 
